0107bec8dd96e0a2948521c049da8cef7f380f0a
Nishan Bishwokarma <nishanbishokarma98@gmail.com>
2023-07-11 17:44:26 +0545

    Array operations added

---
+++ Arrays/largest_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n[10], i, large;
+    printf("Enter 10 numbers: \n");
+
+    for (i = 0; i < 10; i++)
+    {
+        scanf("%d", &n[i]);
+    }
+
+    large = n[0];
+
+    for (i = 1; i < 10; i++)
+    {
+        if (large < n[i])
+        {
+            large = n[i];
+        }
+    }
+    
+    printf("The largest number is %d.", large);
+    return 0;
+}
