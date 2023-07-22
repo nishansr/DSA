a9ab991b62f349cd2c51855902ffcf202611fc2d
Nishan Bishwokarma <nishanbishokarma98@gmail.com>
2023-07-22 19:56:15 +0545

    ascending order added

---
+++ Arrays ADT/10_ascending.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n[10], i, j, temp;
+    printf("Enter ten numbers: \n");
+    for (i = 0; i < 10; i++)
+    {
+        scanf("%d",&n[i]);
+    }
+    
+    for (i = 0; i < 10; i++)
+    {
+        for (j = i+1; j < 10; j++)
+        {
+            if(n[i]>n[j])
+            {
+                temp = n[i];
+                n[i] = n[j];
+                n[j] = temp;
+            }
+        }
+        
+    }
+    
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d\t",n[i]);
+    }
+
+    return 0;
+}
