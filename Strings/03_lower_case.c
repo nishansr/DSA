354d61fbb9dcf2ca0e701e60a27d1931bf4afe33
Nishan Bishwokarma <nishanbishokarma98@gmail.com>
2023-08-01 15:46:31 +0545

    lower case added

---
+++ Strings/03_lower_case.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void lower(char *a)
+{
+    int i;
+    for (i = 0; a[i] != '\0'; i++)
+    {
+        if (a[i] >= 64 && a[i] <= 90)
+        {
+            a[i] += 32;
+        }
+    }
+}
+int main()
+{
+    char a[10], res;
+    printf("Enter your string: ");
+    scanf("%s", &a);
+
+    lower(a);
+
+    printf("The lower case of the string is %s.", a);
+
+    return 0;
+}
